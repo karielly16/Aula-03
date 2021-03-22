d01109474b62b5129f4979d2aebcdee9d01545a9
karielly16 <79888115+karielly16@users.noreply.github.com>
2021-03-22 18:56:20 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+$(document).ready(
+    function () {
+        alert("o documento carregou")
+    }
+
+)
+
+$("#bt_inicio").click(
+    function () {
+        alert('Opa, você clicou no botão inicio, tenha cuidado')
+    }
+)
+
